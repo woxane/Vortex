b6f5594f9d1f44304b5baa27637cdeb0995a124e
woxane <minavandmohsen@gmail.com>
2023-07-12 16:43:04 +0330

    Change .gitignore and Write CheckConfig function in main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+
+
+
+def ConfigCheck() : 
+    if not os.path.exists('config.py') : 
+        InstaUsername = input('Enter your Instagram Username : ')
+        InstaPass = input('Enter your Instagram Password : ')
+
+        with open('config.py' , 'w') as File : 
+            File.write(f'InstaUsername = "{InstaUsername}"')
+            File.write(f'InstaPass = "{InstaPass}"')
+
+
+
+
+if __name__ == '__main__' : 
+    ConfigCheck()
