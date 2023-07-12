a9b6b5aced7624bc1ad095373dce5fc2197b8460
woxane <minavandmohsen@gmail.com>
2023-07-12 18:13:59 +0330

    write CheckPost fun in InstApi.py

--- instaApi.py
+++ instaApi.py
@@ -14,7 +14,19 @@ class InstagramAPI :
         # Login
         self.User = Client()
         self.User.login(self.Username , self.Password )
+        
+    def CheckPost(self) : 
+        UnreadDirects = self.User.direct_threads(selected_filter = 'unread')
+
+        for Direct in UnreadDirects : 
+            # if the last post in DM is Video  :
+            if Direct.messages[0].item_type == 'clip' :
+                # seen the Direct for not consider the direct again
+                self.User.direct_send_seen(thread_id = Direct.id)
+                # give the data like this ( user_id , url ) for authintication we need ... 
+                yield (Direct.id , ''.join(Direct.messages[0].clip.video_url))
 
 
-Page = InstagramAPI
 
+Page = InstagramAPI()
+print(list(Page.CheckPost()))
