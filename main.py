07d64d63323eae3e95192972a03e44b1a2f68463
woxane <minavandmohsen@gmail.com>
2023-07-13 08:26:37 +0330

    get user number for telegram in create config func

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-import instaApi
 from time import sleep
 
 
@@ -7,6 +6,10 @@ class Main :
 
     def __init__(self): 
         ConfigCheck() 
+    
+        # First check for config because in instaApi we import config 
+        import instaApi 
+
         self.Page = instaApi.InstagramAPI()
         
     def CheckDMs(self): 
@@ -24,11 +27,12 @@ def ConfigCheck() :
     if not os.path.exists('config.py') : 
         InstaUsername = input('Enter your Instagram Username : ')
         InstaPass = input('Enter your Instagram Password : ')
+        TelNumber = input('Enter your Telegram Number ( START WITH + ) : ')
         # Writing config file 
         with open('config.py' , 'w') as File : 
             File.write(f'InstaUsername = "{InstaUsername}"')
             File.write(f'\nInstaPass = "{InstaPass}"')
-
+            File.write(f'\nTelNumber = "{TelNumber}"')
 
 
 
