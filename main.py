3898546278546f6959c4ead02999199342a6a891
woxane <minavandmohsen@gmail.com>
2023-07-13 08:51:21 +0330

    get ApiId ApiHash and Token in main.py

--- main.py
+++ main.py
@@ -27,13 +27,16 @@ def ConfigCheck() :
     if not os.path.exists('config.py') : 
         InstaUsername = input('Enter your Instagram Username : ')
         InstaPass = input('Enter your Instagram Password : ')
-        TelNumber = input('Enter your Telegram Number ( START WITH + ) : ')
+        ApiId = int(input('Enter your Telegram Bot Api Id : '))
+        ApiHash = input('Enter you Telegram Bot Api Hash : ' )
+        Token = input('Enter you Telegram Bot Token : ')
         # Writing config file 
         with open('config.py' , 'w') as File : 
             File.write(f'InstaUsername = "{InstaUsername}"')
             File.write(f'\nInstaPass = "{InstaPass}"')
-            File.write(f'\nTelNumber = "{TelNumber}"')
-
+            File.write(f'\nApiId = "{ApiId}"')
+            File.write(f'\nApiHash = "{ApiHash}"')
+            File.write(f'\nToken = "{Token}"')
 
 
 if __name__ == '__main__' : 
