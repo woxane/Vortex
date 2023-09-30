90d7d89d2617d4e9875f0f19d8a172ebd4d50010
woxane <mohsen.minavand1384@gmail.com>
2023-09-30 12:10:33 +0330

    Take User function into User.py

---
+++ utils/telegram/User.py
@@ -0,0 +1,11 @@
+from __init__ import Cursor
+
+def Exists(TelUserId) : 
+    Cursor.execute(f'select TelUserId from Info')
+    # Cursor.fecthall output is like this : [(1,) , (2,)]
+    # with above code i convert it to this : [1,2]
+    UserIdsList = list(map(lambda x : x[0] , Cursor.fetchall()))  
+    return TelUserId in UserIdsList
+
+def AddUser(TelUserId) : 
+    Cursor.execute(f'insert into Info (TelUserId , Access , Active , Admin) values ({TelUserId} , 1 , 0 ,0)')
