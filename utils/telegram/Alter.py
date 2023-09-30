481f22c25494d482da49f9a9c8f069a1efd8ba6e
woxane <mohsen.minavand1384@gmail.com>
2023-09-30 12:01:32 +0330

    take Alters function in events into Alter.py with dependencies in __init__.py

---
+++ utils/telegram/Alter.py
@@ -0,0 +1,7 @@
+from __init__ import Cursor 
+
+def Access(TelUserId , Access) : 
+    Cursor.execute(f'update Info set Access = {Access} where TelUserId = {TelUserId}')
+
+def Admin(TelUserId , Permission) : 
+    Cursor.execute(f'update Info set Admin = {Permission} where TelUserId = {TelUserId}')
