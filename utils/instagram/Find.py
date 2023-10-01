487ffe1bc56b988a7ec666b285af5de242ed08dc
woxane <mohsen.minavand1384@gmail.com>
2023-10-01 10:45:04 +0330

    Create Functions based InstaUserId and TelegramUserId

---
+++ utils/instagram/Find.py
@@ -0,0 +1,17 @@
+from utils.instagra.__init__ import Cursor
+
+def TelUserId(InstaUserId) :
+    Cursor.execute(f'select TelUserId from Info where InstaUserId = {InstaUserId}') 
+    TelUserId = Cursor.fetchone()
+
+    if TelUserId :
+        return TelUserId[0]
+    return TelUserId
+
+def InstaUserId(TelUserId) :
+    Cursor.execute(f'select InstaUserId from Info where TelUserId = {TelUserId}') 
+    InstaUserId= Cursor.fetchone()
+
+    if InstaUserId :
+        return InstaUserId[0]
+    return InstaUserId
