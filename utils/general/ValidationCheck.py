3101534ca2ae91a219639140e64d4769212de0f3
woxane <mohsen.minavand1384@gmail.com>
2023-10-01 16:18:34 +0330

    Create env file checker

--- utils/general/ValidationCheck.py
+++ utils/general/ValidationCheck.py
@@ -1,3 +1,7 @@
+import json
+from os import getenv
+from dotenv import load_dotenv
+
 def JsonFile(Path) :
     try : 
         with open(Path , 'r') as File : 
@@ -7,3 +11,8 @@ def JsonFile(Path) :
 
     except : 
         return False
+
+def Env(Path) : 
+    load_dotenv()
+    return all((getenv('InstaUsername') , getenv('InstaPass') \
+            ,getenv('ApiId') ,getenv('ApiHash') , getenv('Token')))
