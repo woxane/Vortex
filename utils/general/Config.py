a204c7583cf446c7cd31cb7c63c4d3903ae6dfb0
woxane <mohsen.minavand1384@gmail.com>
2023-10-01 16:51:19 +0330

    Edit env file function created

--- utils/general/Config.py
+++ utils/general/Config.py
@@ -1,3 +1,6 @@
+from os import getenv 
+import dotenv 
+
 def CreateEnv(Path) : 
     InstaUsername = input('Enter your Instagram Username : ')
     InstaPass = input('Enter your Instagram Password : ')
@@ -5,12 +8,27 @@ def CreateEnv(Path) :
     ApiHash = input('Enter you Telegram Bot Api Hash : ' )
     Token = input('Enter you Telegram Bot Token : ')
     # Writing config file 
-    with open('../.env' , 'w') as File : 
+    with open(Path , 'w') as File : 
         File.write(f'InstaUsername={InstaUsername}')
         File.write(f'\nInstaPass={InstaPass}')
         File.write(f'\nApiId={ApiId}')
         File.write(f'\nApiHash={ApiHash}')
         File.write(f'\nToken={Token}')
+
+def EditEnv(Path) : 
+    dotenv.load_dotenv()
+    Data = {'InstaUsername' : getenv('InstaUsername') , 
+            'InstaPass' : getenv('InstaPass') , 
+            'ApiId' : getenv('ApiId') ,
+            'ApiHash' : getenv('ApiHash') , 
+            'Token' : getenv('Token') }
+
+    for Name , Value in Data.items() : 
+        print(Name + ' : ' + Value) 
+        NewValue = input(f'Enter your new Value for {Name} : (Enter for dont changing)')
+        if NewValue : 
+            dotenv.set_key(Path , Name ,NewValue )
+
 
 def CreateJson(Path , Data) :
     with open(Path , 'w') as File : 
