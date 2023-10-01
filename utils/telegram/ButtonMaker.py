c410872eed6f0ead90673d5a1ea2da010ed38fb4
woxane <mohsen.minavand1384@gmail.com>
2023-10-01 12:25:26 +0330

    minor changes

--- utils/telegram/ButtonMaker.py
+++ utils/telegram/ButtonMaker.py
@@ -1,6 +1,6 @@
 from telethon.sync import Button
 
-def Inline(DataList , DoneMessage) :
+def Inline(DataList , DoneMessage = None) :
     # i want to inline buttons seprate two by two for this : 
     
     Buttons = list(map(lambda DataIndex : [Button.inline(DataList[DataIndex]) , Button.inline(DataList[DataIndex + 1])] \
@@ -13,7 +13,7 @@ def Inline(DataList , DoneMessage) :
 
     return Buttons
 
-def Url(Names , Links , DoneMessage) :
+def Url(Names , Links , DoneMessage = None) :
     Buttons = list(map(lambda Index : [Button.url(Names[Index] , Links[Index]) , Button.url(Names[Index + 1] , Links[Index + 1])] \
             if Index + 1 != len(Links) else [Button.url(Names[Index] , Links[Index])] ,\
             range(len(Links))[::2] ))
