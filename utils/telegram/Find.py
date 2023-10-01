1bbd51cc44c1eeffd3006c2561630905edc5b45f
woxane <mohsen.minavand1384@gmail.com>
2023-10-01 12:21:17 +0330

    Find Admins User Id function added

--- utils/telegram/Find.py
+++ utils/telegram/Find.py
@@ -6,3 +6,8 @@ def Users() :
 
     return UserIds
 
+def Admins() : 
+    Cursor.execute('select TelUserIds from Info where Admin = 1')
+    AdminsId = list(map(lambda x : x[0] , Cursor.fetchall()))
+
+    return AdminsId 
