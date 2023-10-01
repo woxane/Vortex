cd452578a11c0ac7dc5b031a9f2064576c96bb6c
woxane <mohsen.minavand1384@gmail.com>
2023-10-01 16:09:14 +0330

    CreateEnv function create

---
+++ utils/general/Config.py
@@ -0,0 +1,13 @@
+def CreateEnv(Path) : 
+    InstaUsername = input('Enter your Instagram Username : ')
+    InstaPass = input('Enter your Instagram Password : ')
+    ApiId = int(input('Enter your Telegram Bot Api Id : '))
+    ApiHash = input('Enter you Telegram Bot Api Hash : ' )
+    Token = input('Enter you Telegram Bot Token : ')
+    # Writing config file 
+    with open('../.env' , 'w') as File : 
+        File.write(f'InstaUsername={InstaUsername}')
+        File.write(f'\nInstaPass={InstaPass}')
+        File.write(f'\nApiId={ApiId}')
+        File.write(f'\nApiHash={ApiHash}')
+        File.write(f'\nToken={Token}')
