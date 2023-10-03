eac54a37e91e998c89174296b9ecf3f5f8e23146
woxane <mohsen.minavand1384@gmail.com>
2023-10-03 10:50:23 +0330

    Create function for both lanuage

---
+++ handlers/__init__.py
@@ -0,0 +1,15 @@
+
+
+def StartEn() :
+    LanguageSet = 'Please select the language you want to set 🗣'
+    HeyAdmin = 'Hey Admin 🤵'
+    Hi = "Hi"
+    Activate = 'Your account is not active .\nplease activate your account with /activate .'
+    JoinChannel = 'You must join to above channels before using the bot 🚷. \nClick ✅ after join the channel . '
+
+def StartFa() : 
+    LanguageSet = '🗣 لطفا زبان مد نظر خود را برای ادامه انتخاب کنید '
+    HeyAdmin = '🤵 سلام ادمین'
+    Hi = "سلام"
+    Activate = 'اکانت شما اکتیو نیستش . \n لطفا برای اکتیو کردن از /active استفاده کنید . '
+    JoinChannel = '🚷 شما قبل از استفاده از ربات باید در چنل های زیر جوین باشید . \n کلیک کنید .  ✅ بعد از جوین شدن بر روی  '
