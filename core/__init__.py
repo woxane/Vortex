e85e17fbf1ed161e2c16d5b506656a3b013871db
woxane <mohsen.minavand1384@gmail.com>
2023-10-05 11:19:57 +0330

    Messages added in Fa/En

---
+++ core/__init__.py
@@ -0,0 +1,16 @@
+def MessagesEn() : 
+    Messages = {
+        'Done' : 'Done ✅' , 
+        'GroupWarning' : 'The Group username that you provided is no longer valid ❌\n**Maybe the bot is no longer a member of that chat**' ,
+        'JoinWarning' : "You Can't Access the bot until you joined the Sponsors Channel 🚷" , 
+        'BanWarning' : 'Your Account is BANNED 🚷' , 
+    }
+
+
+def MessagesFa() : 
+    Messages = {
+        'Done' : '✅ انجام شد' , 
+        'GroupWarning' : '❌ یوزرنیم گروهی که به ما ارائه دادید دیگر معتبر نیست\n**👤 شاید ربات در گروه شما دیگر عضو نباشد **' ,
+        'JoinWarning' : "🚷 شما نمیتوانید به بات دسترسی داشته باشید تا وقتی که در چنل های اسپانسر عضو نباشید " , 
+        'BanWarning' : '🚷 اکانت شما بن شده است ' , 
+    }
