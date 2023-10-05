741e28ee8c4f65f67d0d5ecb178dad5968bf2d26
woxane <mohsen.minavand1384@gmail.com>
2023-10-05 10:55:10 +0330

    Instagram Messages created in fa/en

--- utils/instagram/__init__.py
+++ utils/instagram/__init__.py
@@ -2,3 +2,17 @@ import sqlite3
 
 _Connection = sqlite3.connect('../database/Vortex.db' , isolation_level = None , check_same_thread = False)
 Cursor = _Connection.cursor()
+
+def MessagesEn() : 
+    Messages = {
+            'ActivateWarning' : 'Your Account is not Activated 🚷' ,
+            }
+
+    return Messages
+
+def MessagesFa() : 
+    Messages = {
+            'ActivateWarning' : 'اکانت شما اکتیو نمیباشد 🚷' , 
+            }
+
+    return Messages
