1df481dee1f49b8afe9b67cf43c06ee320a2f710
woxane <mohsen.minavand1384@gmail.com>
2023-10-07 14:15:54 +0330

    Check the pattern of proxy before send request

--- utils/Proxy.py
+++ utils/Proxy.py
@@ -1,6 +1,11 @@
 import requests
+import re
 
-def Check(Proxy) : 
+def Check(Proxy) :
+    ProxyValidation = re.match(r'^https?://\S+:\d+$' , Proxy)
+    if not ProxyValidation : 
+        print("The structure of proxy isn't correct .\nThe correct way : http(s)://exmaple:example")
+        exit()
     Proxies = {
             'http' : 'http://' + Proxy , 
             'https' : 'https://' + Proxy
