02161bcc1eb6511ecdbc09c8a933a5c2247a3d23
woxane <mohsen.minavand1384@gmail.com>
2023-10-07 14:11:32 +0330

    Add proxy flag

--- utils/general/Flag.py
+++ utils/general/Flag.py
@@ -1,7 +1,9 @@
 from utils.general import Config
+from utils import Proxy
 
-def Check(Flag) : 
-    AvilableFlags = '-E'
+def Check(Flags) : 
+    AvilableFlags = '-E -P'
+    Flag = Flags[1]
     if Flag not in AvilableFlags: 
         print('This Flag is not exists .')
         print('exiting ... ')
@@ -10,3 +12,10 @@ def Check(Flag) :
     elif Flag == '-E' : 
         Config.EditEnv('../.env')
     
+    elif Flag == '-P' :
+        try : 
+            Proxy_ = Flags[2]
+            Proxy.Check(Proxy_) 
+
+        except : 
+            print("There isn't any input for proxy ")
